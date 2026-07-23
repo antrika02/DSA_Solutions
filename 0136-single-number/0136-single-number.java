e0adf518e51fe84a6d962d18f3882473481c756b
Antrika Kashyap <146320403+antrika02@users.noreply.github.com>
2026-07-23 16:19:19 +0530

    Time: 533 ms (6.6%), Space: 46.9 MB (64.99%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,5 +1,9 @@
 class Solution {
     public int singleNumber(int[] nums) {
+        // Brute approach
+
+
+
         int n = nums.length;
         for (int i = 0; i<n; i++){
             int count = 0;
@@ -13,6 +17,19 @@ class Solution {
             }
         }
        return -1; 
-        
+        //  int max = 0;
+        // for(int i = 0; i< n; i++){
+        //     max = Math.max(max, nums[i]);
+        // }
+        // int[] hash_array = new int[max+1];
+        // for(int i = 0; i<n; i++){
+        //     hash_array[nums[i]]++;
+        // }
+        // for(int i = 0; i<n; i++){
+        //     if(hash_array[nums[i]] ==1){
+        //         return nums[i];
+        //     }
+        // }
+        // return -1;
     }
 }
